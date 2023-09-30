a679d71abcd124e07ac8af5747449d5bcefceb92
Daniel Aanensen <diraanensen@gmail.com>
2023-09-30 14:48:47 +0200

    Day 2 finished

--- advent_of_code_2022/day2/day2.py
+++ advent_of_code_2022/day2/day2.py
@@ -1,14 +1,18 @@
 # Points
 win = 6
 draw = 3
-loss = 1
+loss = 0
 
 # Choices
-rock = 1
-paper = 2
-scissor = 3
+rock_points = 1
+paper_points = 2
+scissor_points = 3
 
-my_points = 0
+rock = "A"
+paper = "B"
+scissors = "C"
+
+my_points = []
 
 me = "Z"
 opponent = "C"
@@ -18,36 +22,62 @@ file = open("advent_of_code_2022\day2\input.txt", "r+")
 output = file.read()
 file.close()
 
+def map_xyz_input(input):
+    if input == "X":
+        return rock
+    if input == "Y":
+        return paper
+    if input == "Z":
+        return scissors
+    return input
+
 # Returning values from round
 def do_round(player1, player2):
-    if player1 == "Y" and player2 == "A":
-        return draw + rock
-    if player1 == "Y" and player2 == "B":
-        return loss + rock
-    if player1 == "Y" and player2 == "C":
-        return win + rock
-    if player1 == "X" and player2 == "A":
-        return win + paper
-    if player1 == "X" and player2 == "B":
-        return draw + paper
-    if player1 == "X" and player2 == "C":
-        return loss + paper
-    if player1 == "Z" and player2 == "A":
-        return loss + scissor
-    if player1 == "Z" and player2 == "B":
-        return win + scissor
-    if player1 == "Z" and player2 == "C":
-        return draw + scissor
+    player1 = map_xyz_input(player1)
+    player2 = map_xyz_input(player2)
+
+    points = 0
+
+    # Apply Choice points
+    if player1 == rock:
+        points += rock_points
+
+    if player1 == paper:
+        points += paper_points
+
+    if player1 == scissors:
+        points += scissor_points
+
+    # Check for draw
+    if player1 == player2:
+        points += draw
+        
+    # Check for win
+    if player1 == rock and player2 == scissors:
+        points += win
+    if player1 == paper and player2 == rock:
+        points += win
+    if player1 == scissors and player2 == paper:
+        points += win
+
+    # Check for loss
+    if player1 == rock and player2 == paper:
+        points += loss
+    if player1 == paper and player2 == scissors:
+        points += loss
+    if player1 == scissors and player2 == rock:
+        points += loss
     
-    raise ValueError("Invalid choice")
-    
+    return points
 
 for round in output.split("\n"):
     if (round == ""):
-        print("Warning: emptly line")
+        print("Warning: empty line")
         continue
 
-    print(do_round(round[2], round[0]))
-    my_points += do_round(me, opponent)
-    
-    
+    my_move = round[2]
+    opponent_move = round[0]
+
+    my_points.append(do_round(my_move, opponent_move)) 
+
+print("According to this strategy I would have", sum(my_points))
