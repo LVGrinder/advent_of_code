b12cbbbe2382ba5d5d116bf2fa3ca2f8abf4de7c
Daniel Aanensen <diraanensen@gmail.com>
2023-09-30 16:20:49 +0200

    Day3

--- advent_of_code_2022/day3/part1.py
+++ advent_of_code_2022/day3/part1.py
@@ -6,7 +6,12 @@ file.close()
 
 print(output)
 
-alphabet = "0 a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z"
+priority = []
 
-priority = alphabet.split("\n")
+alphabet = "0 a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z"
+alphabet = alphabet.split(" ")
+
+for i in range(len(alphabet)):
+    priority.append([i, alphabet[i]])
+
 print(priority) 
