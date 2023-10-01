d9b32bcc3cfabf2442e1c968a1c0d97b2418e776
Daniel Aanensen <diraanensen@gmail.com>
2023-10-01 16:01:10 +0200

    Day4 part 1

--- advent_of_code_2022/day4/part1.py
+++ advent_of_code_2022/day4/part1.py
@@ -9,4 +9,33 @@ pairs = output.split()
 
 
 
-print(pairs)
+
+#print(pairs)
+
+# Create range with first number with the last number
+
+def format_person(person):
+    # Create list of range
+    person = person.split("-")
+
+    # Convert to int
+    person = list(map(int, person))
+    return person
+
+
+changes_needed = 0
+
+for pair in pairs:
+    pair = pair.split(",")
+    person1 = format_person(pair[0])
+    person2 = format_person(pair[1])
+
+    # 2 - 8
+    # 3 - 6
+
+
+    if (person1[0] <= person2[0] and person1[1] >= person2[1]) or (person2[0] <= person1[0] and person2[1] >= person1[1]):
+        print("\n\n", person1, "\n", person2)
+        changes_needed += 1
+
+    print(changes_needed)
