0f47c0f297d00ba89c13b06114124f8345916a84
Daniel Aanensen <diraanensen@gmail.com>
2023-10-01 16:08:58 +0200

    Day4 part 1

--- advent_of_code_2022/day4/part1.py
+++ advent_of_code_2022/day4/part1.py
@@ -1,24 +1,15 @@
 
-# Opening file
+# Get input so we can use it in the task later
 file = open("advent_of_code_2022\day4\input.txt", "r+")
-output = file.read()
+input = file.read()
 file.close()
 
-
-pairs = output.split()
-
-
-
-
-#print(pairs)
-
-# Create range with first number with the last number
+pairs = input.split()
 
 def format_person(person):
-    # Create list of range
     person = person.split("-")
 
-    # Convert to int
+    # Map list to ints since they are strings
     person = list(map(int, person))
     return person
 
@@ -26,15 +17,16 @@ def format_person(person):
 changes_needed = 0
 
 for pair in pairs:
+    # We're splitting it since the people are seperated by a comma like "24-26,80-42"
     pair = pair.split(",")
+
     person1 = format_person(pair[0])
     person2 = format_person(pair[1])
 
-    # 2 - 8
-    # 3 - 6
+    person1_contains_person2 = person1[0] <= person2[0] and person1[1] >= person2[1]
+    person2_contains_person1 = person2[0] <= person1[0] and person2[1] >= person1[1]
 
-
-    if (person1[0] <= person2[0] and person1[1] >= person2[1]) or (person2[0] <= person1[0] and person2[1] >= person1[1]):
+    if person1_contains_person2 or person2_contains_person1:
         print("\n\n", person1, "\n", person2)
         changes_needed += 1
 
