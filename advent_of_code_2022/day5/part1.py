21bf3d1d5a28862397ef7823e4896989be825e12
Daniel Aanensen <tetochrono@protonmail.com>
2024-02-29 15:18:02 +0100

    finally progress

--- advent_of_code_2022/day5/part1.py
+++ advent_of_code_2022/day5/part1.py
@@ -1,4 +1,3 @@
-
 with open("input.txt", "r+") as f:
     lines = f.readlines()
 
@@ -6,5 +5,13 @@ crates = lines[:9]
 
 instructions = lines[9:]
 
-print(instructions)
+# print(instructions)
 
+amount = 1
+for row in crates:
+    amount += 1
+    print(row)
+    # row = row.split()
+    # print(row)
+    for i in range(0, len(row), 4):
+        print(row[i + 1])
