ace91a472a1931782dabab25bcfe43cefda9a6cd
Daniel Aanensen <tetochrono@protonmail.com>
2024-03-03 16:23:53 +0000

    part2 done

--- advent_of_code_2022/day6/part2.py
+++ advent_of_code_2022/day6/part2.py
@@ -1,14 +1,11 @@
 import os
 from pathlib import Path
 
-with open(Path(os.getcwd(), "advent_of_code_2022", "day6", "example.txt"), "r+") as f:
+with open(Path(os.getcwd(), "advent_of_code_2022", "day6", "input.txt"), "r+") as f:
     lines = f.readlines()
 
 databuffer: str = lines[0]
 print(databuffer)
-
-startofstring: int = 0
-endofstring: int = 4
 
 bufferlist: list = []
 matchinglist: list = []
