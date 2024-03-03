908a2fd56b75b46b783445e15773a4b61031bf4a
Daniel Aanensen <tetochrono@protonmail.com>
2024-03-03 12:28:21 +0000

    getting somewhere on day6

--- advent_of_code_2022/day6/part1.py
+++ advent_of_code_2022/day6/part1.py
@@ -10,11 +10,30 @@ print(databuffer)
 startofstring: int = 0
 endofstring: int = 4
 
+matchlist: list = []
+secondarylist: list = []
+
 for i in range(len(databuffer)):
     print(i)
-    startofstring = +1
-    endofstring = +1
 
     packet = databuffer[i : i + 4]
 
     print(packet)
+
+    matchlist = []
+    for letter in packet:
+        print(letter)
+        matchlist.append(letter)
+
+    print(matchlist)
+    times_checked: int = 0
+    for letter in matchlist:
+        secondarylist = matchlist
+        secondarylist.pop(times_checked)
+        print(f"current letter being checked: {letter}")
+        print(f"current existing list: {secondarylist}")
+        times_checked = +1
+
+    #     if letter == letter and letter:
+    #         print(f"found: {packet}")
+    #         break
