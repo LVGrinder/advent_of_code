5208c88da666b0696c36553952e5c23a6c5242cc
Daniel Aanensen <tetochrono@protonmail.com>
2024-03-04 12:58:09 +0000

    forced to use union bcs | isn't working correctly

--- advent_of_code_2022/day7/part1.py
+++ advent_of_code_2022/day7/part1.py
@@ -1,4 +1,5 @@
 import os
+from typing import Union
 from pathlib import Path
 from pydantic import BaseModel
 
@@ -14,12 +15,23 @@ class File(BaseModel):
 
 
 class Folder(BaseModel):
-    contents: dict[str, "Folder | File"]
+    contents: dict[str, Union[File, "Folder"]]
     name: str
 
-    def add_item(self, item: "Folder | File"):
+    def add_item(self, item: Union[File, "Folder"]):
         self.contents[item.name] = item
 
+    def get_size(self):
+        total_size = 0
+        for item in self.contents:
+            if isinstance(item, File):
+                total_size += item.size
+            elif isinstance(item, Folder):
+                total_size += item.get_size()
+        return total_size
+
+
+Folder.model_rebuild
 
 for line in lines:
     line = line.replace("$ ", "")
