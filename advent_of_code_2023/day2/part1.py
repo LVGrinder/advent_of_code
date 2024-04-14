64c547879a5ad735ddbf8f1edcbb9965a81bfcbf
Daniel Aanensen <tetochrono@protonmail.com>
2024-04-14 19:04:50 +0000

    day 2 part 1

--- advent_of_code_2023/day2/part1.py
+++ advent_of_code_2023/day2/part1.py
@@ -5,6 +5,16 @@ from pathlib import Path
 with open(Path(os.getcwd(), "advent_of_code_2023", "day2", "input.txt"), "r+") as f:
     lines = f.readlines()
 
+id: int = 0
 
-for line in lines:
-    print(line)
+for game in lines:
+    # print(line)
+
+    id += 1
+    color: list[str] = re.findall(r"(\d\s[a-z]+)", game)
+    print(color)
+
+    for cube in color:
+        print(cube)
+        number_of_cubes = cube.split()
+        print(number_of_cubes)
